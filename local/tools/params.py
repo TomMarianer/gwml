ab1e1bebe153d8717dc9a85b819888d895a5fb82
Tom Marianer <tommaria@mail.tau.ac.il>
2020-09-10 09:51:15 +0300

    added kde parameters to local params file added compute_kde.ipynb added kde and contours files to local tools

--- local/tools/params.py
+++ local/tools/params.py
@@ -3,5 +3,19 @@
 parameters file for local
 """
 
+### model parameters
+
 condition_method = 'gs_wrap_no_ty'
 model = 'new/resnet152v2/adadelta/gpu_test_15'
+
+### kde parameters
+
+# map grid ranges
+x_range = (-16, 22) 
+y_range = (-25, 21.5)
+
+# estimator parameters
+kernel = 'gaussian'
+bandwidth = 0.3
+
+kde_th = -11.5 # kde method threshold

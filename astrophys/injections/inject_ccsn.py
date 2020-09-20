c135f9f8b1938687cd7ebc496b4b470df97dc81f
Tom Marianer <tommaria@mail.tau.ac.il>
2020-09-20 18:35:36 +0300

    fixed inject_times.py and inject_ccsn.py to read new columns of sky_loc_csv

--- astrophys/injections/inject_ccsn.py
+++ astrophys/injections/inject_ccsn.py
@@ -36,7 +36,7 @@ params_path = Path(git_path + '/shared/injection_params')
 inj_df = pd.read_csv(join(params_path, 'soft_inj_time.csv'), usecols=['H']) #usecols=[detector])
 # inj_df = pd.read_csv(join(params_path, 'soft_inj_time_no_constraint.csv'), usecols=['H']) #usecols=[detector])
 
-sky_loc = pd.read_csv(join(params_path, 'sky_loc_csv.csv'), usecols=['ra', 'dec'])
+sky_loc = pd.read_csv(join(params_path, 'sky_loc_csv.csv'), usecols=['ra', 'dec', 'pol'])
 
 start_time = time.time()
 
--- astrophys/injections/inject_times.py
+++ astrophys/injections/inject_times.py
@@ -35,7 +35,7 @@ params_path = Path('../../shared/injection_params')
 inj_df = pd.read_csv(join(params_path, 'soft_inj_time.csv'), usecols=[detector])
 # inj_df = pd.read_csv(join(params_path, 'soft_inj_time_no_constraint.csv'), usecols=[detector])
 
-sky_loc = pd.read_csv(join(params_path, 'sky_loc_csv.csv'), usecols=['ra', 'dec'])
+sky_loc = pd.read_csv(join(params_path, 'sky_loc_csv.csv'), usecols=['ra', 'dec', 'pol', 'alpha'])
 
 start_time = time.time()
 
